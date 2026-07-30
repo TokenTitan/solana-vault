f217279fbc1f7238021610cc80aa3d298d2047c8
Rish <sharmarish272@gmail.com>
2026-07-30 10:45:00 +0530

    Fix Anchor workspace build

--- tests/vault.ts
+++ tests/vault.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day2Vault } from "../target/types/day2_vault";
 
-describe("day2_vault", () => {
+describe("vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
